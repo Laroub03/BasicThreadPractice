58fa444e67cb53940d85e605857d0f1b458348fc
Laroub03 <ayoublaroub@gmail.com>
2023-04-17 20:02:05 +0200

    Initial commit

---
+++ BasicThreadPractice/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using BasicThreadPratice;
+
+namespace BasicThreadPratice
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            // Start two threads to print two strings
+            program.ShowStrings();
+
+            // Start a thread to simulate temperature readings and alarms
+            program.Temperature();
+
+            // Start two threads to read user input and display asterisks
+            program.ShowKeys();
+
+            Console.Read();
+        }
+
+        private void ShowStrings()
+        {
+            // Create an instance of ThreadProgram
+            ThreadProgram threadProgram = new ThreadProgram();
+
+            // Create a thread to execute the String method of ThreadProgram
+            Thread thread = new Thread(new ThreadStart(threadProgram.String));
+            thread.Name = "ShowStr";
+            thread.Start();
+
+            // Create another thread to execute the String2 method of ThreadProgram
+            Thread thread2 = new Thread(new ThreadStart(threadProgram.String2));
+            thread2.Name = "ShowStr2";
+            thread2.Start();
+        }
+
+        private void Temperature()
+        {
+            // Create an instance of ThreadProgram
+            ThreadProgram threadProgram = new ThreadProgram();
+
+            // Create a thread to simulate temperature readings and alarms
+            Thread temperatureThread = new Thread(new ThreadStart(threadProgram.TemperatureThread));
+            temperatureThread.Name = "TemperatureThread";
+            temperatureThread.Start();
+
+            // Wait for the temperature thread to finish
+            temperatureThread.Join();
+
+            // Print a message to indicate that the temperature thread has terminated
+            Console.WriteLine("Alarm-tråd termineret!");
+        }
+
+        private void ShowKeys()
+        {
+            // Create an instance of ThreadProgram
+            ThreadProgram threadProgram = new ThreadProgram();
+
+            // Create a thread to read user input
+            Thread getKeys = new Thread(new ThreadStart(threadProgram.Read));
+            getKeys.Name = "GetKeys";
+            getKeys.Start();
+
+            // Create another thread to display asterisks
+            Thread showCharThread = new Thread(new ThreadStart(threadProgram.Print));
+            showCharThread.Name = "ShowCharThread";
+            showCharThread.Start();
+        }
+    }
+}
---
+++ BasicThreadPractice/ThreadProgram.cs
@@ -0,0 +1,74 @@
+﻿// This code defines a namespace named BasicThreadPratice that contains a class named ThreadProgram.
+using System;
+namespace BasicThreadPratice
+{
+    // This is the class ThreadProgram.
+    public class ThreadProgram
+    {
+        // This field is used to hold the current character to be displayed.
+        char asterisk = '*';
+
+        // This method displays a string "C#-trådning er nemt!" 5 times with a 1-second delay between each output.
+        public void String()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("C#-trådning er nemt!");
+                Thread.Sleep(1000);
+            }
+        }
+
+        // This method displays a string "Også med flere tråde ..." 5 times with a 1-second delay between each output.
+        public void String2()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(" Også med flere tråde …");
+                Thread.Sleep(1000);
+            }
+        }
+
+        // This method generates a random temperature and displays it. If the temperature is greater than 100 or less than 0, an alarm message is displayed.
+        public void TemperatureThread()
+        {
+            Random random = new Random();
+            int alarmCount = 0;
+            while (alarmCount < 3)
+            {
+                Thread.Sleep(2000);
+                int temperature = random.Next(-20, 120);
+                Console.Write($"Temperature: {temperature}°C");
+                if (temperature > 100 || temperature < 0)
+                {
+                    Console.Write(" ALARM!");
+                    alarmCount++;
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // This method reads a single character from the console input and stores it in the asterisk field.
+        public void Read()
+        {
+            string strInput;
+            bool allowedInputs;
+            while (true)
+            {
+                strInput = Console.ReadLine();
+                allowedInputs = char.TryParse(strInput, out this.asterisk);
+                if (allowedInputs == false)
+                    this.asterisk = '*';
+            }
+        }
+
+        // This method displays the current value of the asterisk field on the console.
+        public void Print()
+        {
+            while (true)
+            {
+                Console.Write(this.asterisk);
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
